e80d0a892663a9acae7228f017896eaaf5167f7f
Anthony Van Horick <anthony.vanhorick@student.ap.be>
2020-10-16 13:14:41 +0200

    Renamed game, refactored code.

--- GameDevelopment/Program.cs
+++ GameDevelopment/Program.cs
@@ -7,7 +7,7 @@ namespace GameDevelopment
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new Platformer())
                 game.Run();
         }
     }
