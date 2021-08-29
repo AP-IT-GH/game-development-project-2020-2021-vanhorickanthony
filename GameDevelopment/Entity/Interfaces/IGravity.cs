25f21732136d0df9ae4fc3af7d615bb2f91d0df3
Anthony Van Horick <anthony.vanhorick@student.ap.be>
2021-08-29 19:17:24 +0200

    Implemented interface for gravity.

---
+++ GameDevelopment/Entity/Interfaces/IGravity.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameDevelopment.Entity.Interfaces
+{
+    public interface IGravity
+    {
+        Vector2 Gravity { get; set; }
+    }
+}
