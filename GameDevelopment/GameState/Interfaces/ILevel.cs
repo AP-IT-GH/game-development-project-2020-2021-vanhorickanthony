0cb8387a1163ef92eed6540be11b1dc539346861
Anthony Van Horick <anthony.vanhorick@student.ap.be>
2021-09-05 21:38:25 +0200

    Level interface now also holds logic related to adding or setting specific layers.

--- GameDevelopment/GameState/Interfaces/ILevel.cs
+++ GameDevelopment/GameState/Interfaces/ILevel.cs
@@ -1,15 +1,20 @@
-using System;
-using GameDevelopment.Core;
-using GameDevelopment.Entity;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
+
+using GameDevelopment.Entity;
+
 using MonoGame.Extended.Tiled;
 
 namespace GameDevelopment.GameState.Interfaces
 {
     public interface ILevel
     {
+        public void SetCollisionLayer(TiledMapTileLayer mapLayer);
+
+        public void SetObjectiveLayer(TiledMapTileLayer mapLayer);
+        
         public void AddMapLayer(TiledMapTileLayer mapLayer);
+
+        public void AddLethalMapLayer(TiledMapTileLayer mapLayer);
 
         public void AddNpc(Hostile_NPC npc, Vector2 destination);
     }
